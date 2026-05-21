82689c86fcba6aa04518348bbdcb7237596f46db
saad <mail.saad0011@gmail.com>
2026-05-21 17:01:38 +0500

    Add additional emoji examples to demonstration

---
+++ Ch_01/emoji_char.py
@@ -0,0 +1,16 @@
+''' 
+    We can print an emoji in terminal by using its Unicode
+    Follow this site for Unicodes: https://unicode.org/emoji/charts/full-emoji-list.html
+    Add backslash before the code and replace + with 000 
+'''
+
+
+# Example
+print("Laughing: \U0001F923")
+print("Winking: \U0001F609")
+
+
+'''
+    NOTE: Terminal support for emojis varies by OS and font settings.
+    - If characters do not render, ensure your terminal supports UTF-8 encoding.
+'''
