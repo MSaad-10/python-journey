78fa0146835f6609d7e8a961181d1da2abf59c4c
saad <mail.saad0011@gmail.com>
2026-05-21 16:14:41 +0500

    print() function

---
+++ Ch_01/print_func.py
@@ -0,0 +1,22 @@
+print('hello world')    # used 'single quotes' because 'hello world' is a string
+print("hello world")    # used "double quotes" because "hello world" is a string
+
+print('a')              # used 'single quotes' because 'a' is a character
+print("a")              # used "double quotes" because "a" is a character
+
+print(5)                # 5 is an integer therefore no single or double quotes required
+print(3.14)             # 3.14 is a float therefore no single or double quotes required
+
+# It's OK to use 'single quotes' inside "double quotes"
+print("hello 'beautiful' world")
+# can't use "double quotes" inside "double quotes"
+# print("hello "ugly" world")   ❌
+# print("hello \"ugly\" world")  ✅
+# print('I'm Saad')   ❌ This will cause a SyntaxError
+print('I\'m Saad')    # ✅ Using the escape character '\'
+
+# It's OK to use "double quotes" inside 'single quotes'
+print('hello "beautiful" world')
+# can't use 'single quotes' inside 'single quotes'
+# print('hello 'ugly' world')   ❌
+# print('hello \'ugly\' world')   ✅
