5866bf7011e5307f6d1f5e1e263cd6079e68bab0
saad <mail.saad0011@gmail.com>
2026-06-29 16:41:06 +0500

    feat: add escape sequence practice examples

---
+++ Ch_01/Ex_01.py
@@ -0,0 +1,13 @@
+''' =========== Print the following outputs on Terminal =========== '''
+
+# Output: this is double \\ backslash
+print("this is double \\\\ backslash")
+
+# Output: this is /\/\/\/\/\ mountain
+print("this is /\\/\\/\\/\\/\\ mountain")
+
+# Output: he is     awesome (use escape sequence instead of manual spaces)
+print("he is \t awesome")
+
+# Output: \" \n \t \' 
+print("\\\" \\n \\t \\\' ")
