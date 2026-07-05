ed31f81257570401c220ae8b62643a637ceb8b9c
saad <mail.saad0011@gmail.com>
2026-07-05 15:03:34 +0500

    feat: exercise solution for generating a dictionary of cubes up to n

---
+++ Ch_07/Ex_01.py
@@ -0,0 +1,18 @@
+'''
+    - Define a function that
+        * takes a number(n) as input
+        * returns a dictionary containing cube of numbers from 1 to n
+        * Example:
+            Input: 3
+            Output: {1:1, 2:8, 3:27} 
+'''
+
+
+def cube_finder(n):
+    cubes = {}
+    for i in range(1,n+1):
+        cubes[i] = i**3
+    return cubes
+
+n = int(input("Enter a number: "))
+print(f'Answer: {cube_finder(n)}')
